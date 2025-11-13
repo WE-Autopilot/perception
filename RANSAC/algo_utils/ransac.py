a686f8453d53bdd1f1255193068938db55bb250c
Tygo Crawley <tcrawley@uwo.ca>
2025-11-12 22:51:29 -0500

    test plane fn, estimate fn fix,and WIP visualize ipynb

--- RANSAC/algo_utils/ransac.py
+++ RANSAC/algo_utils/ransac.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 class RANSAC:
     # When an initial estimate is provided
-    def RANSAC(data, initial_estimate, estimate_fn, test_fn, max_retry=10, thresh=0.1):
+    def RANSAC_fn(self, data: np.ndarray, initial_estimate, estimate_fn, test_fn, max_retry=10, thresh=5):
 
         # Evaluate the initial estimate and return if good enough
         bestEstimate = initial_estimate
@@ -10,6 +12,7 @@ class RANSAC:
 
         # Otherwise, proceed with RANSAC iterations
         for i in range(max_retry):
+            print(i)
             estimate = estimate_fn(data)
             score = test_fn(data, estimate)
 
@@ -18,10 +21,11 @@ class RANSAC:
             if score < bestError:
                 bestEstimate = estimate
                 bestError = score
+            
         return bestEstimate
 
     # When no initial estimate is provided
-    def RANSAC_noInit(data, estimate_fn, test_fn, thresh, max_retry):
+    def RANSAC_noInit(self, data, estimate_fn, test_fn, thresh, max_retry):
 
         bestEstimate = None
         bestError = float('inf')
