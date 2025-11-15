7ffba355ad9382790d983373fe1e3f0a8e1a3b34
DexterLuth <dluth@uwo.ca>
2025-11-15 11:04:20 -0500

    heightCutoff for points in point cloud

---
+++ data/removeOutliers.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# Remove outliers based on Z-axis threshold
+def removeOutliers(data, threshold=0.6):
+
+    points = data["points"]
+    zMin, zMax = np.min(points[:, 2]), np.max(points[:, 2])
+    zRange = zMax - zMin
+    zUpperBound = zMin + (threshold * zRange)
+
+    # Mask for points below the upper bound
+    mask = np.where(points[:, 2] <= zUpperBound)[0]  # [0] to just get indicies
+    data["points"] = points[mask]
+
+    return data
+
+
+file_path = "../test_data/test104.npy"
+data = np.load(file_path, allow_pickle=True).item()
+cleaned_data = removeOutliers(data.copy())
+np.save("../test_data_cleaned/test104_cleaned.npy", cleaned_data)
+
+
+# print(data["points"].shape)
+# print(cleaned_data["points"].shape)
+# print(data["points"][0:10])
+# print(cleaned_data["points"][0:10])
