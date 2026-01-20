f7d8beaed54a54e4615f564545fe0a575673e952
DexterLuth <dluth@uwo.ca>
2026-01-20 14:59:21 -0500

    running ransac with static test nparray

--- ros2/src/mapping_interface/mapping_interface/groundPlaneNode.py
+++ ros2/src/mapping_interface/mapping_interface/groundPlaneNode.py
@@ -2,11 +2,14 @@
 import rclpy
 from rclpy.node import Node
 
+
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs_py.point_cloud2 as pc2
 
 # from .RANSAC.algo_utils import ransac
 from .RANSAC.plane_utils import estimate
+from .RANSAC.plane_utils import test
+from .RANSAC.algo_utils import ransac
 import numpy as np
 
 
@@ -15,21 +18,28 @@ class groundPlaneNode(Node):
         super().__init__("my_node")
         self.get_logger().info("Node started!")
 
-        test1 = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-        result = estimate.estimate_plane(test1)
-        print(result)
-
         # subscription to pointcloud topic
         pointCloudSub = self.create_subscription(
             PointCloud2, "pointcloud", self.callback, 10
         )
 
     def callback(self, msg):
-        # Convert PointCloud2 to Python list of points
-        points_list = list(
-            pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
+        points = []
+        for p in pc2.read_points(msg, skip_nans=True):
+            points.append([p[0], p[1], p[2]])
+        array = np.array(points, dtype=np.float32)
+
+        # just using a static np array rn for testing
+        test1 = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
+
+        ransacEstimation = ransac.RANSAC_noInit(
+            data=test1,
+            estimate_fn=estimate.estimate_plane,
+            test_fn=test.test_plane,
+            thresh=5,
+            max_retry=10,
         )
-        print("Points:", points_list)
+        print(ransacEstimation)
 
 
 def main(args=None):
